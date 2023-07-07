d3bff2d52caabea045fa566d5b0c80218d80bea8
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-07-07 19:43:27 +0200

    master

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,31 +1,11 @@
 <?php
-    namespace App\Models;
 
-    class Listing {
-        public static function all() {
-            return [
+namespace App\Models;
 
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'Lorem ipsum'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'Lorem ipsum 2 = nice ?'
-                ]
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-            ];
-        }
-
-        public static function find($id) {
-            $listings = self::all();
-
-            foreach ($listings as $listing) {
-                if ($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
+}
