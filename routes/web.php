d298422846379754a17cd2c16e6583a6d7a05b24
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-07-10 01:01:17 +0200

    master

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,12 @@ use App\Models\Listing;
 // all listings
 Route::get('/', [ListingController::class, 'index']);
 
+// show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//store listing
+Route::post('/listings', [ListingController::class, 'store']);
+
 // single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
